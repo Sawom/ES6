ee4d1b745b9da576e5cab4ace4f4f0d60493b0f2
Abdur Rashid Sawom <64780532+Sawom@users.noreply.github.com>
2022-01-02 19:52:03 +0600

    Add files via upload

---
+++ templateString.js
@@ -0,0 +1,8 @@
+// `` this is backtick
+const name1 = 'abdur rashid';
+const lastName = 'sawom';
+const string1 = `30th Anniversary of PAC-MAN ${name1}. May 21, 2010. When I was growing up, · 
+When I was growing up, my dad had the best job I could possibly imagine: he was a ${lastName}. ` ;
+console.log(name1);
+console.log(lastName);
+console.log(string1);
